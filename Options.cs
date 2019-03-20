af713a5c8eb1be3eec68557f91eadfa780e87b0c
pedoc <pedoc@qq.com>
2019-03-20 12:11:49 +0800

    添加项目文件。

---
+++ Options.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace GitChangeFIleExactor
+{
+    public class Options
+    {
+        [Option('r', Required = true, HelpText = "仓库所在目录")]
+        public string RepoDir { get; set; }
+        [Option('c', Required = true, HelpText = "提交Id(8位)或Sha，支持多个值")]
+        public IEnumerable<string> Commits { get; set; }=new string[]{};
+
+        [Option('o', Required = false, HelpText = "输出目录，如果不指定则在程序目录下")]
+        public string OutputDir { get; set; }
+
+        public bool Check()
+        {
+            if (!Directory.Exists(RepoDir))
+            {
+                Console.WriteLine("输入的文件夹不存在");
+                return false;
+            }
+
+            if (!Commits.Any())
+            {
+                Console.WriteLine("未指定任何提交标识");
+                return false;
+            }
+
+            if (string.IsNullOrEmpty(OutputDir))
+            {
+                OutputDir = Path.Combine(Environment.CurrentDirectory, DateTime.Now.ToString("yyyy-mm-dd HH fff"));
+            }
+            if (Directory.Exists(OutputDir))
+            {
+                Console.WriteLine($"指定的输出目录已存在，可能导致文件覆盖 {OutputDir}");
+            }
+            else Directory.CreateDirectory(OutputDir);
+            
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using LibGit2Sharp;
+
+namespace GitChangeFIleExactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(option =>
+                {
+                    if (option.Check())
+                    {
+                        var repo = new Repository(option.RepoDir);
+                        var waitCommits = new List<Commit>();
+                        foreach (var commit in repo.Commits)
+                        {
+                            if (option.Commits.Contains(commit.Id.ToString(8).ToString())
+                            || option.Commits.Contains(commit.Id.ToString()))
+                            {
+                                waitCommits.Add(commit);
+                            }
+                        }
+                        var orderdWaitCommits= waitCommits.OrderBy(item => item.Committer.When).ToList();//按照提交时间升序，确保如果一个文件在多次commit中被修改，使用最后的版本
+                        Console.WriteLine($"输入总 commitId数：{option.Commits.Count()}，实际发现数：{orderdWaitCommits.Count()}，详情如下（按时间升序），请确认是否继续(y/n)?");
+                        orderdWaitCommits.ForEach(item =>
+                        {
+                            Console.WriteLine($"{item.Id}:{item.MessageShort}");
+                        });
+                        var input = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(input) && input.Equals("y", StringComparison.OrdinalIgnoreCase))
+                        {
+                            Console.WriteLine();
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            foreach (var commit in orderdWaitCommits)
+                            {
+                                var parentCommit = commit.Parents.First();
+                                var patchs = DiffCommit(repo, parentCommit.Id, commit.Id);
+                                foreach (var patch in patchs)
+                                {
+                                    var realPath = Path.Combine(option.RepoDir, patch.Path);
+                                    var outputPath = Path.Combine(option.OutputDir, patch.Path);
+                                    var outputPathDir = Path.GetDirectoryName(outputPath);
+                                    if (outputPathDir != null && !Directory.Exists(outputPathDir)) Directory.CreateDirectory(outputPathDir);
+                                    if (patch.Status != ChangeKind.Deleted)
+                                    {
+                                        if(File.Exists(outputPath)) Console.WriteLine($"文件已存在，将被覆盖({outputPath})");
+                                        File.Copy(realPath, outputPath, true);
+                                        Console.WriteLine($"已拷贝变化文件({realPath})至({outputPath})");
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine($"{patch.Path} 状态为已删除，自动跳过");
+                                    }
+                                }
+                            }
+                        }
+                        else Console.WriteLine("输入有误");
+                    }
+                    Console.WriteLine("按任意键退出");
+                    Console.ReadKey();
+                }).WithNotParsed(error =>
+                {
+                    Console.WriteLine($"参数输入有误，无法完成任务");
+                    Console.ReadKey();
+                });
+        }
+
+        private static Patch DiffCommit(IRepository repo, ObjectId oldId, ObjectId newId)
+        {
+            var commitLeft = repo.Lookup<Commit>(oldId);
+            var commitRight = repo.Lookup<Commit>(newId);
+            Console.WriteLine($"正在比对差异 [old]{commitLeft.Message} - [new]{commitRight.Message}");
+            var patch = repo.Diff.Compare<Patch>(commitLeft.Tree, commitRight.Tree);
+            return patch;
+        }
+    }
+}
